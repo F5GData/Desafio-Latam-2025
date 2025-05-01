a7927fac625fdf922eb778c9d59c781833206aa1
F5GData <pipesa2810@gmail.com>
2025-05-01 19:45:43 -0400

    day1

---
+++ Programación/30DaysOfPython/DAY-01.py
@@ -0,0 +1,66 @@
+'''
+2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+    addition(+)
+    subtraction(-)
+    multiplication(*)
+    modulus(%)
+    division(/)
+    exponential(**)
+    floor division operator(//)
+'''
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+'''
+3.Write strings on the python interactive shell. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+'''
+print('Felipe')
+print('Pipe')
+print('Chile')
+print('I enjoy 30 days of python')
+
+
+'''
+Check the data types of the following data:
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']
+Your name
+Your family name
+Your country
+'''
+type(10)
+type(9.8)
+type(3.14)
+type(4 - 4j)
+type(['Asabeneh', 'Python', 'Finland'])
+type('Felipe')
+type('Pipe')
+type('Chile')
+
+
+'''
+Exercise: Level 3
+Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+
+'''
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
